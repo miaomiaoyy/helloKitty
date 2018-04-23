e78a87e69facf57098fdf5aa82fd47e7c4dd5260
yykathleen224 <yykathleen224@gmail.com>
2018-04-22 21:54:22 -0700

    heruko test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,5 +6,5 @@
 export const environment = {
   production: false,
   // baseUrl: 'http://localhost:3100'
-  baseUrl: 'https://hellokittyy.herokuapp.com/'
+  baseUrl: 'https://hellokittyy.herokuapp.com'
 };
